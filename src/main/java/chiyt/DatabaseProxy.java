1d890b50c96e10b38568bea9c4ffae51453f063f
chiyt27 <hipatricia27@gmail.com>
2025-06-22 11:24:28 +0800

    OOD, OOP完成

--- src/main/java/chiyt/DatabaseProxy.java
+++ src/main/java/chiyt/DatabaseProxy.java
@@ -16,13 +16,13 @@ public class DatabaseProxy implements Database {
 
     @Override
     public Employee getEmployeeById(int id) {
-        if(this.realDatabase==null){
-            lazyInitialization();
-        }
         /*需求B4 密碼保護 (Password Protection)：在存取 RealDatabase 的 getEmployeeId 操作時，
         必須在實際存取前先檢查執行環境變數中 PASSWORD 變數的值，確定該值必須為 1qaz2wsx 後才得以繼續，否則中斷此存取*/
         String password = System.getenv("PASSWORD");
         if ("1qaz2wsx".equals(password)) {
+            if(this.realDatabase==null){
+                lazyInitialization();
+            }
             Employee realEmployee = realDatabase.getEmployeeById(id);
             if (realEmployee != null) {
                 return new EmployeeProxy(realEmployee, this); // 返回 EmployeeProxy，以便達成需求B3 延遲載入subordinates清單
